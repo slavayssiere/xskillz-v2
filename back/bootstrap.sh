45faa9f0bff683202b2a276e8472eaec7f5b501c
Sebastien Lavayssiere <slavayssiere@sebastiens-mbp.home>
2018-04-09 11:36:43 +0200

    update docker-compose for local dev and remove crisp

--- back/bootstrap.sh
+++ back/bootstrap.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 set -e
 
+until nc -w5 $RDS_HOST 3306
+do
+  sleep 0.5
+  echo "Wait for MySQL"
+done
+sleep 1
+
 echo "DB migrate..."
 node_modules/db-migrate/bin/db-migrate up --env kubernetes
 echo "DB migrated"
